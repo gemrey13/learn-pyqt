c86f5c0b03e704f42186b691cebb6247a8e32e14
gemrey13 <gemreyranola@gmail.com>
2025-11-24 13:38:00 +0800

    images

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("My PyQt5 Application")
+        self.setGeometry(700, 300, 500, 500)
+
+        label = QLabel("Hello, PyQt5!", self)
+
+        sampleImg = QLabel(self)
+        sampleImg.setGeometry(0, 0, 250, 300)
+        pixmap = QPixmap("test.png")
+        sampleImg.setPixmap(pixmap)
+        sampleImg.setScaledContents(True)
+
+        sampleImg.setGeometry((self.width() - sampleImg.width()) // 2,
+                              (self.height() - sampleImg.height()) // 2,
+                             sampleImg.width(),
+                             sampleImg.height())
 
 def main():
     app = QApplication(sys.argv)
